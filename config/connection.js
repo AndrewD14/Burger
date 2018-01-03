a658bc2658ee026debd95421e0559dd1b72de9be
AndrewD14 <Leingod2005@gmail.com>
2018-01-03 15:28:55 -0600

    Changed to using a connection pool.

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,30 @@
 //import the npm packages
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
     host     : 'us-cdbr-iron-east-05.cleardb.net',
     user     : 'bc1b5f16727246',
     password : 'ded30105',
-    database : 'heroku_3d6bd26e6ff3156'
+    database : 'heroku_3d6bd26e6ff3156',
+    connectionLimit: 1
   });
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+var getConnection = function(){
+  pool.getConnection(function(error, connection){
+    if(error){
+      console.error("error connecting: " + error);
+      return;
+    }
+    return connection;
+  });
+}
+
+//closes the connection
+var closeConnection = function(connection){
+  connection.release();
+}
 
 // Export connection for our ORM to use.
-module.exports = connection;
+module.exports = {getConnection: getConnection,
+  closeConnection: closeConnection};
--- config/orm.js
+++ config/orm.js
@@ -1,26 +1,36 @@
 //imports the connection config
-var connection = require('./connection.js');
+var pool = require('./connection.js');
 
 var orm = {
     selectAll:function(table, func){
+        var connection = pool.getConnection();
+
         connection.query("SELECT * FROM ??", table, function(error, data){
             if(error) throw error;
 
             func(data);
+
+            pool.closeConnection(connection);
         });
     },
     insertOne: function(table, cols, values, func){
+        var connection = pool.getConnection();
+
         connection.query("INSERT INTO ?? (??) VALUES (?)", [table, cols, values], function(error, data){
             if(error) throw error;
 
             func(data);
+            pool.closeConnection(connection);
         });
     },
     updateOne: function(table, values, id, func){
+        var connection = pool.getConnection();
+
         connection.query("UPDATE ?? SET ? WHERE id = ?", [table, values, id], function(error, data){
             if(error) throw error;
 
             func(data);
+            pool.closeConnection(connection);
         });
     }
 }
