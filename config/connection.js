15a5e81f12e1a72b6b1b1bb277479a91bd77c00b
AndrewD14 <Leingod2005@gmail.com>
2018-01-03 18:44:26 -0600

    Changing the connection.js to use process.env for cleardb

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,12 @@
 //import the npm packages
 var mysql = require('mysql');
 
-var pool = mysql.createPool({
+var pool;
+
+if(process.env.CLEARDB_DATABASE_URL)
+  pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
+else
+  pool = mysql.createPool({
     host     : 'us-cdbr-iron-east-05.cleardb.net',
     user     : 'bc1b5f16727246',
     password : 'ded30105',
